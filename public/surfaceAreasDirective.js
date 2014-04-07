eac4e4fbf4b41f42af01d0e955924f439279601a
Blake La Pierre <blakelapierre@gmail.com>
2014-04-07 15:12:06 -0700

    cool $scope stuff with <input type="range"> controls :)

--- public/surfaceAreasDirective.js
+++ public/surfaceAreasDirective.js
@@ -1,21 +1,44 @@
-var angular = require('angular');
+var angular = require('angular'),
+	_ = require('lodash');
 
 module.exports = function() {
 	return {
 		restrict: 'E',
 		template: require('./surfaceAreasTemplate.html'),
 		controller: function($scope) {
-			$scope.characters = [];
+			$scope.start = createNumberScope(0);
+			$scope.range = createNumberScope(256);
+			$scope.end = $scope.start.value + $scope.range.value;
 
-			var start = 0xa0,
-				range = 0xff,
-				end = start + range;
+			$scope.updateCharacters = _.debounce(function() {
+				console.log('updating');
+				var start = $scope.start.value,
+					range = $scope.range.value,
+					end = start + range;
 
-			for (var i = start; i < end; i++) {
-				$scope.characters.push({
-					value: String.fromCharCode(i)
-				});
-			}
+				$scope.end = end;
+				$scope.characters = [];
+
+				for (var i = start; i < end; i++) {
+					$scope.characters.push({
+						value: String.fromCharCode(i)
+					});
+				}
+
+				$scope.$apply();
+			}, 30);
+
+			$scope.updateCharacters();
 		}
-	}
+	};
+};
+
+var createNumberScope = function(initialValue) {
+	var value = initialValue;
+	return Object.defineProperties({}, {
+		value: {
+			get: function() { return value; },
+			set: function(v) { value = parseInt(v); }
+		}
+	});
 };
